c6058223c9691e83ee742f06bf871752b1a4bf32
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-08-24 14:23:53 +0200

    Initial commit

---
+++ REALMessageOfTheDay/Constants.cs
@@ -0,0 +1,8 @@
+namespace REALMessageOfTheDay;
+
+public class Constants
+{
+    public const string PluginGuid = "hansolo1000falcon.realmessageoftheday";
+    public const string PluginName = "REALMessageOfTheDay";
+    public const string PluginVersion = "1.0.0";
+}
---
+++ REALMessageOfTheDay/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Net.Http;
+using BepInEx;
+using TMPro;
+using UnityEngine;
+
+namespace REALMessageOfTheDay;
+
+[BepInPlugin(Constants.PluginGuid, Constants.PluginName, Constants.PluginVersion)]
+public class Plugin : BaseUnityPlugin
+{
+    private TextMeshPro motdTmp;
+
+    private string messageOfTheDay;
+
+    private bool canStart;
+
+    private void OnGameInitialized()
+    {
+        motdTmp = GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/motdBodyText")
+            .GetComponent<TextMeshPro>();
+
+        using (HttpClient client = new())
+        {
+            HttpResponseMessage motdResponse = client
+                .GetAsync("https://raw.githubusercontent.com/HanSolo1000Falcon/GorillaInfo/main/MessageOfTheDay.txt")
+                .Result;
+            motdResponse.EnsureSuccessStatusCode();
+
+            using (Stream stream = motdResponse.Content.ReadAsStreamAsync().Result)
+            using (StreamReader reader = new(stream))
+                messageOfTheDay = reader.ReadToEnd();
+
+            canStart = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (!canStart)
+            return;
+        
+        if (motdTmp.text != messageOfTheDay)
+            motdTmp.text = messageOfTheDay;
+    }
+
+    private void Start() => GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+}
